ee9f81212e2fdc07a34ed6d72e443e6c09eefb95
suyuye95 <446390252@qq.com>
2018-03-12 09:54:32 +0800

    最后一次提交

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,33 @@
 var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+var compilesass = require('gulp-sass');
+var htmlmin = require('gulp-htmlmin');
+var mincss = require('gulp-clean-css');
+
+gulp.task('compileSass', function() {
+    gulp.src('./css/style.scss')
+        .pipe(compilesass())
+        .pipe(gulp.dest('./dist/'))
+})
+
+//压缩html
+var options = {
+    removeComments: true,
+    collapseWhitespace: true,
+    collapseBooleanAttributes: true,
+    removeEmptyAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    minifyJS: true,
+    minifyCSS: true
+}
+gulp.task('minHtml', function() {
+    gulp.src('./index.html')
+        .pipe(htmlmin(options))
+        .pipe(gulp.dest('./html/'))
+})
+gulp.task('minCss', function() {
+    gulp.src('./css/style.css')
+        .pipe(mincss())
+        .pipe(gulp.dest('./dist/'))
+})
